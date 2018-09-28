4c2d5e924a40e0f29d2586759ad5e0358a2af0df
student <student@cpe-172-100-19-58.twcny.res.rr.com>
2018-09-28 14:09:41 +0800

    this is so sad alexa play whatever u want

--- rps/ViewController.swift
+++ rps/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnPaper: UIButton!
+    
+    @IBOutlet weak var btnScissors: UIButton!
+    @IBOutlet weak var btnRock: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
