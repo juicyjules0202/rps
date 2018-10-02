347697521d9629a8f5c39a3b5c7d45a53520920e
student <student@cpe-172-100-19-58.twcny.res.rr.com>
2018-10-02 13:58:37 +0800

    reinitialised countdown, no buttons yet

--- rps/ViewController.swift
+++ rps/ViewController.swift
@@ -9,16 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var playerInput = 0
+    var randImput = 0
+    var playerWinsv = 0
+    var randWins = 0
 
-    @IBOutlet weak var btnPaper: UIButton!
-    
-    @IBOutlet weak var btnScissors: UIButton!
-    @IBOutlet weak var btnRock: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
